230ee900c4ef252fa062d5d1a4d0eba263b02b81
Cameron Sun <cameron.csun@gmail.com>
2014-02-11 14:23:06 -0500

    Now possible to enable listening for all connections with --deploy

--- site.rb
+++ site.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 
-set :port, 80
+
+# If script is run with --deploy, server will listen
+# for all incoming connections
+def network_setup
+	set :port, 80
+	if (ARGV[0] and ARGV[0] == '--deploy')
+		set :bind, 0.0.0.0
+	end
+end
+
+network_setup
 
 get '/'  do
 	'More to come'
