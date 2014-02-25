f6f345c5db289e22981d5a15dd7141d99d161eff
Cameron Sun <cameron.csun@gmail.com>
2014-02-25 01:04:15 -0500

    I think octokit has some bugs - some content isn't brought down that would be brought down from api calls (I've checked). Made a solution for the github links not working.

--- work_fetcher.rb
+++ work_fetcher.rb
@@ -11,7 +11,8 @@ module WorkFetcher
 	@cache = RequestsCache.new @client
 
 	def self.repos
-		@cache.handle :repos, self.query_repos
+		without_html_url = @cache.handle :repos, self.query_repos
+		self.add_html_url without_html_url
 	end
 
 	def self.organizations
@@ -30,6 +31,13 @@ module WorkFetcher
 		etag = @cache.get_etag(:request_type)
 		etag = '' if !etag
 		{'If-None-Match' => etag}
+	end
+
+	def self.add_html_url(repos)
+		repos.each do |repo|
+			repo.url = 'https://github.com/' + @login +'/' + repo.name
+		end
+		repos
 	end
 
 end
